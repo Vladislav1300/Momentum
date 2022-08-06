43a38aec0a0909099f22ff9f2dc1cc512931939c
Vladislav1300 <askerko1997@gmail.com>
2022-08-06 19:45:50 +0300

    feat: add track switching function

--- js/audio.js
+++ js/audio.js
@@ -2,24 +2,30 @@ import playList from './playList.js';
 console.log(playList);
 
 let isPlay = false;
+let playNum = 0;
 
 const playlistInner = document.querySelector('.play-list');
 const playBtn = document.querySelector('.play');
+const playPrevBtn = document.querySelector('.play-prev');
+const playNextBtn = document.querySelector('.play-next');
 const audio = new Audio();
 
 function playAudio() {
-    if (!isPlay) {
-        audio.src = playList[0].src;
+    if (isPlay === true) {
+        audio.src = playList[playNum].src;
         audio.currentTime = 0;
         audio.play();
-        isPlay = true;
-        playBtn.classList.add('pause');
+        console.log(playList[playNum].title)
     }
     else {
-        isPlay = false;
         audio.pause();
-        playBtn.classList.remove('pause');
     }
+}
+
+function toggleBtn (){
+    playBtn.classList.toggle('pause');
+    isPlay = !isPlay;
+    console.log(isPlay)
 }
 
 for(let i = 0; i < playList.length; i++) {
@@ -27,6 +33,27 @@ for(let i = 0; i < playList.length; i++) {
     li.classList.add('play-item');
     li.textContent = `${playList[i].title}`;
     playlistInner.append(li);
-  }
+}
 
+function playNext (){
+    if (isPlay){
+        playNum += 1;
+        if (playNum > playList.length - 1) playNum = 0;
+        playAudio();
+        console.log(isPlay)
+    }
+}
+function playPrev (){
+    if (isPlay){
+        playNum -= 1;
+        if (playNum < 0) playNum = playList.length - 1;
+        playAudio();
+        console.log(playNum)
+    }
+}
+
+
+playPrevBtn.addEventListener('click', playPrev);
+playNextBtn.addEventListener('click', playNext);
+playBtn.addEventListener('click', toggleBtn);
 playBtn.addEventListener('click', playAudio);
